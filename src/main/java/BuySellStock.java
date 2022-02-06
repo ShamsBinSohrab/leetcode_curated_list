3ce216dc0b316c2e3489ef2d7ae8f7b7cd683e04
shams <aniknishan@gmail.com>
2022-02-06 14:01:14 +0600

    Reformat

--- src/main/java/BuySellStock.java
+++ src/main/java/BuySellStock.java
@@ -1,6 +1,5 @@
 /** Problem: https://leetcode.com/problems/best-time-to-buy-and-sell-stock/ */
 public class BuySellStock {
-
   public int maxProfit(int[] prices) {
     var profit = 0;
     var buy = prices[0];
--- src/main/java/MaximumSubarray.java
+++ src/main/java/MaximumSubarray.java
@@ -1,7 +1,5 @@
-
 /** Problem: https://leetcode.com/problems/maximum-subarray/ */
 public class MaximumSubarray {
-
   public int maxSubArray(int[] nums) {
     var tMx = nums[0];
     var gMx = nums[0];
